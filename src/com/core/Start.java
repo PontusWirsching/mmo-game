e406d88484e08b625bb95dbf1dd3361de9f02a5e
Jacob Duddles <jacob.duddles@live.co.uk>
2014-09-15 19:46:50 +0100

    Signed-off-by: Jacob Duddles <jacob.duddles@live.co.uk>

---
+++ src/com/core/Start.java
@@ -0,0 +1,28 @@
+package com.core;
+
+import com.lss.flasher.LEngine;
+
+public class Start extends LEngine {
+
+	public Start(int width, int height, String frameName) {
+		super(width, height, frameName);
+
+		skipIntro();
+		start();
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	public static void main(String[] args) {
+		new Start(1024, 720, "MMO_GAME");
+	}
+
+}
